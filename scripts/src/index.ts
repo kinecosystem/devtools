65b34a927ca922f19a457ebadbe1636d0bf9dd3a
Doody Parizada <doody.parizada@gmail.com>
2018-10-29 17:42:55 +0200

    script for creating wallets with registration JWT

---
+++ scripts/src/index.ts
@@ -0,0 +1,80 @@
+import { Keypair } from "@kinecosystem/kin.js";
+import * as jsonwebtoken from "jsonwebtoken";
+import axios from "axios";
+
+const PRODUCTION = "https://api.kinmarketplace.com";
+const PLAYGROUND = "https://api.kinplayground.com";
+export type JWTClaims<SUB extends string> = {
+	iss: string; // issuer - the app_id
+	exp: number; // expiration
+	iat: number; // issued at
+	sub: SUB; // subject
+};
+export type JWTContent<T, SUB extends string> = {
+	header: {
+		typ: string;
+		alg: string;
+		kid: string;
+	};
+	payload: JWTClaims<SUB> & T;
+	signature: string;
+};
+export type RegisterJWTPayload = {
+	user_id: string;
+};
+
+export type AuthToken = {
+	token: string;
+	activated: boolean;
+	expiration_date: string;
+	app_id: string;
+	user_id: string;
+	ecosystem_user_id: string;
+};
+
+export type RegisterMarketplaceClientPayload = {
+	device_id: string;
+	wallet_address: string;
+	sign_in_type: "jwt";
+	jwt: string
+};
+export type ApiError = {
+	code: number;
+	error: string;
+	message: string;
+};
+
+async function register(base: string, data: RegisterMarketplaceClientPayload) {
+	const res = await axios.post<AuthToken>(`${base}/v1/users`, data);
+	return res.data;
+}
+
+async function main() {
+	const keypair = Keypair.random();
+	if (process.argv.length < 5) {
+		throw new Error("usage: npm run create-wallet -- <playground|production> <device_id> <JWT>");
+	}
+	const env: "playground" | "production" = process.argv[2];
+	const deviceId = process.argv[3];
+	const jwt = process.argv[4];
+	let marketplace_base: string;
+
+	if (env === "playground") {
+		marketplace_base = PLAYGROUND;
+	} else if (env === "production") {
+		marketplace_base = PRODUCTION;
+	} else {
+		throw new Error("env must be playground or production");
+	}
+
+	const decoded = jsonwebtoken.decode(jwt, { complete: true }) as JWTContent<RegisterPayload, "register">;
+	console.log(`${decoded.payload.user_id}@${deviceId}) address: <${keypair.publicKey()}> secret key: <${keypair.secret()}>`);
+	try {
+		await register(marketplace_base, {device_id: deviceId, sign_in_type: "jwt", wallet_address: keypair.publicKey(), jwt });
+	} catch (err) {
+		const apiError: ApiError = err.response!.data;
+		throw new Error(apiError.message);
+	}
+}
+
+main().then(() => console.log("done!")).catch((err: Error) => console.error(`failed: ${err.message}`));
