b62d3adc2bea5d6d5df344a30d310d5c714258ad
Doody Parizada <doody.parizada@gmail.com>
2018-11-09 13:19:10 +0200

    add create trustline

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,4 +1,4 @@
-import { Keypair } from "@kinecosystem/kin.js";
+import { Keypair, KinNetwork, createWallet } from "@kinecosystem/kin.js";
 import * as jsonwebtoken from "jsonwebtoken";
 import axios from "axios";
 
@@ -32,6 +32,15 @@ export type AuthToken = {
 	ecosystem_user_id: string;
 };
 
+export type ConfigResponse = {
+	blockchain: {
+	 horizon_url: string;
+	 network_passphrase: string;
+	 asset_issuer: string;
+	 asset_code: string;
+	};
+};
+
 export type RegisterMarketplaceClientPayload = {
 	device_id: string;
 	wallet_address: string;
@@ -45,8 +54,13 @@ export type ApiError = {
 };
 
 async function register(base: string, data: RegisterMarketplaceClientPayload) {
-	const res = await axios.post<AuthToken>(`${base}/v1/users`, data);
+	const res = await axios.post<AuthToken>(`${ base }/v1/users`, data);
 	return res.data;
+}
+
+async function getConfig(base: string) {
+	const res = await axios.get<ConfigResponse>(`${ base }/v1/config`);
+	return res.data.blockchain;
 }
 
 async function main() {
@@ -54,7 +68,7 @@ async function main() {
 	if (process.argv.length < 5) {
 		throw new Error("usage: npm run create-wallet -- <playground|production> <device_id> <JWT>");
 	}
-	const env: "playground" | "production" = process.argv[2];
+	const env = process.argv[2];
 	const deviceId = process.argv[3];
 	const jwt = process.argv[4];
 	let marketplace_base: string;
@@ -67,15 +81,24 @@ async function main() {
 		throw new Error("env must be playground or production");
 	}
 
-	const decoded = jsonwebtoken.decode(jwt, { complete: true }) as JWTContent<RegisterPayload, "register">;
-	console.log(`${decoded.payload.user_id}@${deviceId}) address: <${keypair.publicKey()}> secret key: <${keypair.secret()}>`);
+	const decoded = jsonwebtoken.decode(jwt, { complete: true }) as JWTContent<RegisterJWTPayload, "register">;
+	console.log(`${ decoded.payload.user_id }@${ deviceId }) address: <${keypair.publicKey()}> secret key: <${keypair.secret()}>`);
 	try {
-		const authToken = await register(marketplace_base, {device_id: deviceId, sign_in_type: "jwt", wallet_address: keypair.publicKey(), jwt });
+		const authToken = await register(marketplace_base, { device_id: deviceId, sign_in_type: "jwt", wallet_address: keypair.publicKey(), jwt });
 		console.log(`auth token: <${authToken.token}>`);
 	} catch (err) {
 		const apiError: ApiError = err.response!.data;
 		throw new Error(apiError.message);
 	}
+
+	const config = await getConfig(marketplace_base);
+	const network = KinNetwork.from(
+		config.network_passphrase,
+		config.asset_issuer,
+		config.horizon_url);
+
+	const wallet = await createWallet(network, keypair);
+	await wallet.trustKin();
 }
 
 main().then(() => console.log("done!")).catch((err: Error) => console.error(`failed: ${err.message}`));
